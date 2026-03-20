9d21857ef4bfaed7b2bced169b391ca9f13a89c8
maaitlunghau <chunhau.py@gmail.com>
2026-03-20 10:31:23 +0700

    feat(auth): implement jwt token service with refresh token rotation

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9b3d728ecde429be1f6fb5dd5d9574b22dc08c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+826ebe9edf4179eec887cb3de1744281685af037")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
